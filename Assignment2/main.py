1fe4bd1e14a92cdcecaa383b73fbbdb453c50968
Job Ruijters <jobruijters@hotmail.com>
2025-12-22 20:28:33 +0100

    Improved code readability (dont change please)

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -1,34 +1,34 @@
+# Import modules
 import pandas as pd
 import numpy as np
 from pathlib import Path
 
+# Import functions
 from compute_parameters import load_data_routes, comp_distances, comp_yield, comp_hourly_demand, load_data_aicraft, comp_operating_costs
 
-"""
-
-cd "C:/Users/pop_r/OneDrive - Delft University of Technology/Documents/SVV/AirlinePlanningAndOptimisation/Assignment2"
-
-
-"""
-
-# Define paths
+# Define paths of input data
 BASE_DIR = Path(__file__).resolve().parent
 airports_path = BASE_DIR / "Data" / "DemandGroup7.xlsx"
 aircraft_path = BASE_DIR / "Data" / "FleetType.xlsx"
 hours_path    = BASE_DIR / "Data" / "HourCoefficients.xlsx"
 
+# Load parameters from excel files
+destinations, coord, runways, demand, hours = load_data_routes(airports_path, hours_path)
 
-names, coord, runways, demand, hours = load_data_routes(airports_path, hours_path)
-
+# Compute distances between destinations from coordinates
 distance = comp_distances(coord)
 
+# Compute the yield between all airports
 y = comp_yield(distance)
 
+# Compute demand per hour for all routes
 hourly_demand = comp_hourly_demand(demand, hours)
 
+# Load aircraft data for fleet
 speed, seats, TAT, range, runway_req, lease_cost =  load_data_aicraft(aircraft_path)[0:6]
-fleet = load_data_aicraft(aircraft_path)[-1]
+n_ac_in_fleet = load_data_aicraft(aircraft_path)[-1]
 
+# Compute total operating cost per flight leg for each aircraft
 operating_cost = comp_operating_costs(aircraft_path, distance)
 
-print(f"Operating cost: {operating_cost}")
+print(f"operating cost: {operating_cost}")
